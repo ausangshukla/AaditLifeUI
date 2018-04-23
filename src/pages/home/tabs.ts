576cc14da5bce0207a29935f30ce9ae9acfdc1b1
thimmaiah <thimmaiah@gmail.com>
2018-04-23 15:10:19 +0530

    Added tabs

--- src/pages/home/tabs.ts
+++ src/pages/home/tabs.ts
@@ -1,21 +1,33 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Events } from 'ionic-angular';
 import { Angular2TokenService } from 'angular2-token';
 import { HomePage } from './home';
 import { FoodLogs } from '../food-logs/food-logs';
 import { Schedules } from '../schedules/schedules';
+import { ResponseUtility } from '../../providers/response-utility';
 
 @Component({
     templateUrl: 'tabs.html'
-  })
-  export class TabsPage {
+})
+export class TabsPage {
     // this tells the tabs component which Pages
     // should be each tab's root Page
     homeRoot = HomePage;
     foodLogRoot = FoodLogs;
     scheduleRoot = Schedules;
-  
-    constructor() {
-  
+
+    currentUser: any;
+
+    constructor(public respUtility: ResponseUtility,
+        public events: Events,
+        public tokenService: Angular2TokenService) {
+        this.currentUser = tokenService.currentUserData;
+        this.events.subscribe('user:login:success', () => {
+            this.currentUser = tokenService.currentUserData;
+        });
+        this.events.subscribe('user:logout:success', () => {
+            console.log("Hiding tabs as user logged out");
+            this.currentUser = null;
+        });
     }
-  }
+}
